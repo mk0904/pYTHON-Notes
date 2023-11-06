719cce3f46305c3c7d1532230ffcdc6b0a3792dc
mk0904 <148270333+mk0904@users.noreply.github.com>
2023-11-06 14:59:01 +0530

    Update classes and objects.py

--- classes and objects.py
+++ classes and objects.py
@@ -1,1 +1,33 @@
+#map function give map-object as output
+# mao(callable, iterator)
+#also returns an iterator
 
+
+'''input'''
+# class Fraction:
+#     def __init__(self,numerator,denominator):
+#         self.num=numerator
+#         self.deno=denominator
+# frac1=Fraction(2,7)
+# frac2=Fraction(3,8)
+# print(frac1.num)
+# print(frac2.num)
+
+'''output'''
+# 2
+# 3
+
+
+
+# class Fraction:
+#     def __init__(yourself,num,denom):
+#         yourself.num=num
+#         yourself.denom=denom
+#     def add(yourself,fraction2):
+#         return Fraction((yourself.num)*(fraction2.denom) + (fraction2.num)*(yourself.denom), (yourself.denom * fraction2.denom))
+#     def __str__(yourself):
+#         return f"{yourself.num}/{yourself.denom}"
+# fraction1=Fraction(3,7)
+# fraction2=Fraction(2,4)
+# fraction3 = fraction1.add(fraction2)
+# print(fraction3)
